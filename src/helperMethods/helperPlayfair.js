1fbd8ab3545b5a911bc10c6f2f557930b3517941
Mark Suzuki <markdsuzuki@gmail.com>
2018-10-05 15:42:49 -0700

    helper functions for matrix creation

--- src/helperMethods/helperPlayfair.js
+++ src/helperMethods/helperPlayfair.js
@@ -1,8 +1,45 @@
+/************CREATION TABLE FUNCTIONS*************/ 
+
 /**
- * @param {String} [str = ""]
+ * @param {String} 
  * @returns {Array}
+ * @description removes spaces, uppercase all words, maps to remove duplicates and returns keys
  * */ 
 
 function removeDuplicates(str) {
+  let str = str.replace(/\s/g, '').toUpperCase()
   return Object.keys(str.split("").reduce((obj,value) => ({...obj, [value]: 0}), {}))
 }
+
+/** 
+ * @param {String} 
+ * @returns {Array}
+ * @description creates an array with the alphabet excluding Q
+ * */ 
+
+function createAlphaBet(str) {
+  let alphabet = removeDuplicates(str)
+  for (let i = 65; i <= 90; i++) {
+    if (!alphabet.includes(String.fromCharCode(i)) && i !== 81) {
+          alphabet.push(String.fromCharCode(i))
+    }
+  }
+  return alphabet
+}
+
+/** 
+ * @param {String} 
+ * @returns {Array}
+ * @description creates the 5 x 5 table
+ * */ 
+
+function createTable(str) {
+  let alphabet = createAlphaBet(str)
+  let matrix = []
+  for (let i = 0; i < 5; i++) {
+    matrix.push(alphabet.splice(0,5))
+  }
+  return matrix
+}
+
+
