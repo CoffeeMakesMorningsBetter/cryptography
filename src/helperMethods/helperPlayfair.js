3c2fd8e3cc4975f36371386df67aef8b1d7191fe
Mark Suzuki <markdsuzuki@gmail.com>
2018-10-06 20:20:07 -0700

    checking functions

--- src/helperMethods/helperPlayfair.js
+++ src/helperMethods/helperPlayfair.js
@@ -1,27 +1,27 @@
-/************TABLE CREATION FUNCTIONS*************/ 
+/************TABLE CREATION FUNCTIONS*************/
 
 /**
  * @param {String} 
  * @returns {Array}
  * @description removes spaces, uppercase all words, maps to remove duplicates and returns keys
- * */ 
+ * */
 
 function removeDuplicates(str) {
   let str = str.replace(/\s/g, '').toUpperCase()
-  return Object.keys(str.split("").reduce((obj,value) => ({...obj, [value]: 0}), {}))
+  return Object.keys(str.split("").reduce((obj, value) => ({ ...obj, [value]: 0 }), {}))
 }
 
 /** 
  * @param {String} 
  * @returns {Array}
  * @description creates an array with the alphabet excluding Q in order starting with word then rest of alphabet
- * */ 
+ * */
 
 function createAlphaBet(str) {
   let alphabet = removeDuplicates(str)
   for (let i = 65; i <= 90; i++) {
     if (!alphabet.includes(String.fromCharCode(i)) && i !== 81) {
-          alphabet.push(String.fromCharCode(i))
+      alphabet.push(String.fromCharCode(i))
     }
   }
   return alphabet
@@ -31,50 +31,54 @@ function createAlphaBet(str) {
  * @param {String} 
  * @returns {Array}
  * @description creates the 5 x 5 table
- * */ 
+ * */
 
 function createTable(str) {
   let alphabet = createAlphaBet(str)
   let matrix = []
   for (let i = 0; i < 5; i++) {
-    matrix.push(alphabet.splice(0,5))
+    matrix.push(alphabet.splice(0, 5))
   }
   return matrix
 }
 
-/************PAIR CREATION FUNCTION*************/ 
+/************PAIR CREATION FUNCTION*************/
 
 /** 
  * @param {String} 
  * @returns {Array}
  * @description In a playfair cipher the message is split into digraphs, pairs of two letters. If there is an odd number of letters, a Z is added to the last letter. removes spaces and uppercase
- * */ 
+ * */
 
- function pairs(str) {
-    let clearnStr = str.replace(/\s/g, '').toUpperCase()
-    let checkStr = clearnStr.length % 2 === 0 ? clearnStr: `${clearnStr}Z`
-    let pairsArr = []
-    for (let i = 0; i < checkStr.length; i++) {
-      if(i % 2 !== 0) {
-        pairsArr.push(checkStr.slice(i-1,i+1))
-      }
+function pairs(str) {
+  let clearnStr = str.replace(/\s/g, '').toUpperCase()
+  let checkStr = clearnStr.length % 2 === 0 ? clearnStr : `${clearnStr}Z`
+  let pairsArr = []
+  for (let i = 0; i < checkStr.length; i++) {
+    if (i % 2 !== 0) {
+      pairsArr.push(checkStr.slice(i - 1, i + 1))
     }
-    return pairsArr
- }
+  }
+  return pairsArr
+}
 
- /************CHECKING FUNCTIONS*************/ 
+/************CHECKING FUNCTIONS*************/
 
-// NOT ON SAME ROW OR COLUMN FUNCTION
-// CREATE AND RETURN SUBMATRIX 
+function mapIndexPairs(matrix, pairs) {
+  let idx = []
+  for(let i = 0; i < pairs.length; i++) {
+    let coordinatesOne = findValue(matrix, pairs[i][0])
+    let coordinatesTwo = findValue(matrix, pairs[i][1])
+    idx.push([coordinatesOne, coordinatesTwo])
+  }
+  return idx
+}
 
-// COLUMN FUCTION
-  // RETURN ARRAY?
-  // USE RECURSION OR NOT? WHATS MY BASE CASE?
-  // WHERE DO I START?
-
-// ROW FUNCTION
-  // RETURN ARRAY?
-  // WHATS MY BASE CASE? USE RECURSION OR NOT?
-  // WHERE DO I START?
-
-  // get coordinates of both first ie: find them
+// REWRITE RECURSIVELY O(N) vs O(N^2)
+function findValue(arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if(arr[i][j] === value) return [i, j]
+    }
+  }
+}
